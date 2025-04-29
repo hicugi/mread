2ea7deeac3d008d34c79941b44354c7f43723616
Mag <hicugi@gmail.com>
2025-04-29 19:03:27 +0500

    feat; add back button

--- manager/front/src/App.vue
+++ manager/front/src/App.vue
@@ -1,6 +1,14 @@
+<script setup>
+import UiButton from './component/Ui/Button.vue';
+</script>
+
 <template>
   <div>
     <h1>MRead manager</h1>
+
+    <RouterLink v-if="$route.name != 'home'" :to="{ name: 'home' }">
+      <UiButton type="button">Back home</UiButton>
+    </RouterLink>
 
     <main>
       <RouterView />
