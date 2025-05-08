e15c06eb53fd2b901f6f0f86c2e79a94dd7ae899
Mag <hicugi@gmail.com>
2025-05-08 18:29:30 +0500

    feat: add separate buttons for clearing html cache

--- template/src/api.js
+++ template/src/api.js
@@ -3,8 +3,13 @@ import { HOST_URL_KEY } from "./helper/main.js";
 const LOCAL_HOST = "http://127.0.0.1:8000";
 
 export const api = {
-  get: (url) => {
-    const value = (window[HOST_URL_KEY] ?? LOCAL_HOST) + url;
+  host: null,
+
+  get(url) {
+    const host = window[HOST_URL_KEY] ?? LOCAL_HOST;
+    this.host = host;
+
+    const value = host + url;
     return fetch(value).then((res) => res.json());
   }
 };
--- template/src/components/ui/Button.vue
+++ template/src/components/ui/Button.vue
@@ -1,15 +1,27 @@
+<script setup>
+const { variant } = defineProps({
+  variant: {
+    default: 'default',
+  }
+});
+</script>
+
 <template>
-  <button class="ui-button" type="button" @click="$emit('clic')">
+  <button class="ui-button" :class="`ui-button_variant-${variant}`" type="button" @click="$emit('clic')">
     <slot />
   </button>
 </template>
 
 <style>
 .ui-button {
-  margin: 0 auto;
   padding: 12px 15px;
   width: 100%;
   max-width: 210px;
   display: block;
+}
+
+.ui-button_variant-danger {
+  background-color: var(--color-danger);
+  color: white;
 }
 </style>
