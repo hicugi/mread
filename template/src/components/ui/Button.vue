4f253bfebe1913aa2acee521d2a946241b74979f
Mag <hicugi@gmail.com>
2026-04-06 17:35:34 +0500

    update styles for details page

--- template/src/components/ui/Button.vue
+++ template/src/components/ui/Button.vue
@@ -1,22 +1,65 @@
 <script setup>
-const { variant } = defineProps({
+import { computed } from "vue";
+
+const { variant, size } = defineProps({
+  link: {
+    default: null,
+  },
   variant: {
-    default: 'default',
-  }
+    default: "default",
+    variants: ["default", "primary"],
+  },
+  size: {
+    default: "medium",
+    variants: ["medium", "large"],
+  },
 });
+
+const className = computed(() => [
+  "ui-button",
+  `ui-button_variant-${variant}`,
+  `ui-button_size-${size}`,
+]);
 </script>
 
 <template>
-  <button class="ui-button" :class="`ui-button_variant-${variant}`" type="button" @click="$emit('clic')">
+  <RouterLink v-if="link" :to="link" :class="className">
+    <slot />
+  </RouterLink>
+  <button v-else :class="className" type="button" @click="$emit('clic')">
     <slot />
   </button>
 </template>
 
 <style>
 .ui-button {
+  box-sizing: border-box;
   padding: 10px 16px;
   border: none;
-  display: inline-block;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+}
+
+.ui-button_size-large {
+  --height: 60px;
+
+  padding: 16px 24px;
+  border-radius: var(--height);
+  min-height: var(--height);
+  font-size: 18px;
+  font-weight: bold;
+  line-height: 1;
+}
+
+.ui-button_variant-primary {
+  background: #9ba8ff;
+  color: #001c8e;
+}
+.ui-button_variant-default {
+  background: #262626;
+  color: #ADAAAA;
 }
 
 .ui-button_variant-danger {
